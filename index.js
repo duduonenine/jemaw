134e077cf63e001c788ed6009a71455890cd0e41
duduonenine <kidanz.tadesse@gmail.com>
2026-02-19 20:57:39 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,6 @@
+// Inside your login script
+document.getElementById('loginForm').addEventListener('submit', function(e) {
+    e.preventDefault();
+    // For now, let's just redirect
+    window.location.href = "index.html"; 
+});
